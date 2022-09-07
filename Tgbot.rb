7624edba64435bffe3e97101836f293d3d0958ca
Tokway <91993109+Tokway@users.noreply.github.com>
2022-09-07 16:10:50 +0300

    Add files via upload

---
+++ Tgbot.rb
@@ -0,0 +1,47 @@
+#coding: UTF-8
+require 'telegram/bot'
+
+TOKEN = '5787784103:AAFiwqiFz1THL0-HryGVigE5FehKvlqQ3wY'
+
+ANSWERS = [
+    # Позитивні 
+    "Безперечно", 
+    "Вирішено",
+    "Ніяких сумнівів", 
+    "Виразно так", 
+    "Можеш бути впевнений у цьому",
+    # Нерішуче позитивні <<так>>"
+    "Мені здається так",
+    "Хороші перспективи", 
+    "Знаки кажуть - так", 
+    "Так",
+    # Нейтральні 
+    "Поки не ясно, спробуй знову", 
+    "Запитай пізніше", 
+    "Краще не розповідати", 
+    "Зараз не можна передбачити", 
+    "Сконцентруйся і спитай знову",
+    # Негативні 
+    "Навіть не думай", 
+    "Моя відповідь <<ні>>", 
+    "За моїми даними - <<ні>>", 
+    "Перспективи не дуже хороші", 
+    "Дуже сумнівно"
+]
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+    bot.listen do |message|
+        case message.text
+        when '/start', '/start start'
+            bot.api.send_message(
+                chat_id: message.chat.id,
+                text: "Привіт, #{message.from.first_name} я слухаю тебе, запитай мене та я дам відповідь!"
+            )
+        else
+            bot.api.send_message(
+                chat_id: message.chat.id,
+                text: ANSWERS.sample
+            )
+        end
+    end
+end
